bcf2bc21a9963416857312eb177c71712702ffec
mzawadi <mwaranguallan345@gmail.com>
2022-06-25 20:09:45 +0300

    update registry events

--- src/registerName.sol
+++ src/registerName.sol
@@ -1,18 +1,21 @@
 /// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-/// @title A title that should describe the contract/interface
-/// @author The name of the author
-/// @notice Explain to an end user what this does
+/// @title mini-PNS
+/// @author Allan
+/// @notice this is a minimal name registry inspired by ENS built on polygon-mumbai
 /// @dev Explain to a developer any extra details
 
 contract PNSRegistry{
 
     mapping(address => string) registryNames;
 
-    event RegisterName(address account, string );
+    event RegisterName(address account, string registeredName, uint256 datetimestamp);
+    event TransferName(address account, string registeredName , uint256 datetimestamp);
+    event RenewName(address account, string registeredName , uint256 datetimestamp);
+    event RevokeName(address account, string registeredName , uint256 datetimestamp);
 
-    /// @author The name of the author
+    /// @author Allan
     /// @notice Explain to an end user what this does
     /// @dev Explain to a developer any extra details
     constructor(){}
