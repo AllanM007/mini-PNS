873f57202685d780d6cc04dbb2a53dc020b87f01
mzawadi <mwaranguallan345@gmail.com>
2022-06-28 15:30:48 +0300

    update transfer,renew,expire functions

--- src/registerName.sol
+++ src/registerName.sol
@@ -12,6 +12,7 @@ contract PNSRegistry{
         address owner;
         bytes32 name;
         uint registeredDate;
+        uint updateDate;
         bool active;
     }
 
@@ -22,18 +23,18 @@ contract PNSRegistry{
     // mapping(string => bool) activeName;
 
     event RegisterName(address account, bytes32 registeredName, uint256 timestamp);
-    event TransferName(address account, bytes32 registeredName , uint256 timestamp);
+    event TransferName(address account, address recipient, bytes32 registeredName , uint256 timestamp);
     event RenewName(address account, bytes32 registeredName , uint256 timestamp);
     event RevokeName(address account, bytes32 registeredName , uint256 timestamp);
 
     /// @notice Thrown when trying to update a name you don't own
-	error Unauthorized();
+	// error Unauthorized();
 
     /// @notice Thrown when trying to renew a name past due date
-	error PastValidityPeriod();
+	// error PastValidityPeriod();
 
 	/// @notice Thrown when trying to register a name that's already taken
-	error AlreadyRegistered();
+	// error AlreadyRegistered();
 
     /// @author Allan
     /// @notice Explain to an end user what this does
@@ -41,13 +42,18 @@ contract PNSRegistry{
     constructor(){}
 
     function register(address _account, bytes31 _registeredName, uint256 timestamp) public returns (bool){
-        registry = nameRegistry(_account, _registeredName, block.timestamp, true);
 
-        emit RegisterName(_account, _registeredName, timestamp);
-        return true;
+        if (_registeredName == registry.name) {
+            return false;            
+        } else {
+            registry = nameRegistry(_account, _registeredName, block.timestamp, block.timestamp, true);
+            
+            emit RegisterName(_account, _registeredName, timestamp);
+            return true;
+        }
     }
 
-    function verify(bytes32 _registeredName) public view returns(address, uint256, bool){
+    function nameLookup(bytes32 _registeredName) public view returns(address, uint256, bool){
 
         if (_registeredName == registry.name) {
             return (registry.owner, registry.registeredDate, registry.active);
@@ -56,9 +62,35 @@ contract PNSRegistry{
         }
     }
 
-    function transfer(address account, string memory _registeredName) public returns(bool){}
-    function renew(address account, string memory _registeredName) public returns(bool){}
-    function expire(address account, string memory _registeredName) public returns(bool){}
-    function registryLookup() public returns(bytes32){}
+    function transfer(address account, address recipient, bytes32 _registeredName) public returns(bool){
+        if (_registeredName == registry.name && account == registry.owner) {
+            registry.owner = recipient;
+            registry.updateDate = block.timestamp;
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+    function renew(address account, bytes32 _registeredName, uint256 timestamp) public returns(bool){
+        if (_registeredName == registry.name) {
+            require(account == registry.owner, "Unauthorized party");
+            registry.updateDate = timestamp;
+
+            return true;   
+        } else {
+            return false;
+        }
+    }
+    function expire(bytes32 _registeredName) public returns(bool){
+        if (_registeredName == registry.name) {
+            registry.active = false;
+            
+            return true;   
+        } else {
+            return false;
+        }
+    }
+    function registryLookup() public view returns(bytes32){}
 
 }
